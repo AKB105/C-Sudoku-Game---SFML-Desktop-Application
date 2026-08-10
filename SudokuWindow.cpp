d64cab19b85cc098cd095c7df1d3b817248e288a
Angelo Kelly <angelo.kelly05@outlook.com>
2026-08-10 19:51:00 -0400

    Sudoku Game Repo

---
+++ SudokuWindow.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Graphics.hpp>
+
+int main(){
+    sf::RenderWindow cliche_window(sf::VideoMode({800, 600}), "Hello, World!");
+    
+    while (cliche_window.isOpen()){
+        while (const std::optional event = cliche_window.pollEvent()){
+            if (event->is<sf::Event::Closed>()){
+                cliche_window.close();
+            }
+        }
+
+        cliche_window.clear();
+        cliche_window.display();
+    }
+
+    return 0;
+}
